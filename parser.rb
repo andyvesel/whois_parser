a35702b85374e6a8c2fe84a828d615095ce93747
andyvesel <andyvesel@ya.ru>
2015-12-22 14:12:12 +0300

    Small fixes

--- parser.rb
+++ parser.rb
@@ -10,26 +10,22 @@ ws = session.spreadsheet_by_key("1ylT3Wj7WpB2jkwarCvbHoIeM-lQvmBaSN9WjkTp2uw4").
 DOMAIN_REGEX = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/
 
 ws.rows.each_with_index do |row, i|
-  @domain_name = row.first
-  next unless @domain_name.match(DOMAIN_REGEX)
+  domain_name = row.first
+  next unless domain_name.match(DOMAIN_REGEX)
 
-  def process_domain(domain_name, i)
-    @query = Whois.whois(@domain_name)
-    # p @query.domain, i
+  query = Whois.whois(domain_name)
+
+  ip = IPSocket::getaddress(domain_name)
+
+  if i > 0
+    i += 1
+    ws[i, 2] = query.created_on
+    ws[i, 3] = query.expires_on
+    ws[i, 4] = query.admin_contact.to_a[4..9].join(' ') # address
+    ws[i, 5] = query.admin_contact.to_a[10].to_s # phone
+    ws[i, 6] = query.admin_contact[12] # email
+    ws[i, 7] = query.nameservers.join(' ')
+    ws[i, 8] = ip
+    ws.save
   end
-
-  process_domain(@domain_name, i)
-
-  ip = IPSocket::getaddress(@domain_name)
-    if i > 0
-      i += 1
-      ws[i, 2] = @query.created_on
-      ws[i, 3] = @query.expires_on
-      ws[i, 4] = @query.admin_contact[4] # address
-      ws[i, 5] = @query.admin_contact[10] # phone
-      ws[i, 6] = @query.admin_contact[12] # email
-      ws[i, 7] = @query.nameservers[0]
-      ws[i, 8] = ip
-      ws.save
-    end
 end
